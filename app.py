8b1d707ed96d370bf3e2acd74b8e26e01d491950
Devendra Choudhary <devendra072@gmail.com>
2023-07-14 12:13:11 +0530

    second commit

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+import time
+
 from flask import Flask
 from urllib import request
 import http.client
 import json
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/Users/devendra.choudhary/Downloads/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','wav'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1000 * 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif','wav']
+app.config['UPLOAD_PATH'] = 'uploads'
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -173,6 +187,7 @@ def get_product_attribute_with_desc(catalog_id):
 @app.route('/test')
 def test():
     print("received request for test")
+    folder = "/Users/devendra.choudhary/IdeaProjects/monkai/"
     file_path = "/Users/devendra.choudhary/Downloads/Suryavamsam - Amitabh Bachchan Dialogue.wav"
     # audio_text = get_audio_text(file_path)
     audio_text = "This is Designed as per the latest trends to keep you in sync with high fashion and with wedding and other occasion, it will keep you comfortable all day long. The lovely design forms a substantial feature of this wear.It looks stunning every time you match it with accessories.This attractive kurti will surely fetch you compliments for your rich sense of style.Stow away your old stuff when you wear this kurti. Light in weight Daily Wear, Working Wear kurtis will be soft against your skin. Its Simple and unique design and beautiful colours, prints and patterns. Stitched in regular fit, this kurti for women will keep you comfortable all day long. Front Design Looks perfect in this Kurtis. It can be perfect for get together, evening Functions,occasion and party wear as well. We believe in better clothing products cause helping women's to look pretty, feel comfortable is our ultimate goal. If you are not 100% happy, contact to us. before return your Kurti . We don't. We are committed to provide extremely durable readymade kurtis. Pair this kurti with a pair of stylish heels and a matching clutch for a complete casual look for a casual event, a party or an evening with friends. Our collection includes different styles of Kurtis that cater to a wide variety of the wardrobe requirements of the Indian woman. Make a fine addition to your wardrobe by adding this Festive kurti Suits from Bae's Wardrobe. This Kurti Will Give You A Trendy Look With its Beautiful Design coveRed White With Eye-Catching Patterns. Please check size chart in image section to get detailed measurement for choosing perfect size for you."
@@ -181,6 +196,16 @@ def test():
     get_product_attribute_with_desc(catalog_id)
     return "test"
 
+
+def do_processing_of_audio_file(audio_file_name):
+    print("received request for do_processing_of_audio_file")
+    folder = "/Users/devendra.choudhary/IdeaProjects/monkai/"
+    file_path = folder + audio_file_name
+    audio_text = get_audio_text(file_path)
+    search_query = get_search_query(audio_text)
+    catalog_id = get_catalog_id_from_search_query(search_query)
+    text_for_voice = get_product_attribute_with_desc(catalog_id)
+    return text_for_voice
 
 
 @app.route('/upload', methods=['POST'])
@@ -194,18 +219,37 @@ def upload_file():
 
     if file:
         filename = file.filename
-        file.save(filename)
-        return 'File saved successfully.'
+        current_time_str = time.asctime( time.localtime(time.time()))
+        full_file_name = str(current_time_str) + filename
+        file.save()
+        print('File saved successfully.')
+        audio_text = do_processing_of_audio_file(full_file_name)
+        return audio_text
 
 
-@app.route('/handle_form', methods=['POST'])
-def handle_form():
-    print("Posted file: {}".format(request.files['file']))
-    file = request.files['file']
-    return ""
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
+@app.route('/uploadv2', methods=['GET', 'POST'])
+def upload_fileV2():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return ''
 
 if __name__ == '__main__':
     # ssl._create_default_https_context = ssl._create_unverified_context
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
